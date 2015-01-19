c39d403fe00e75b2c8496338f039609d03508f0f
Timothy Mee <tjmee90@gmail.com>
2015-01-19 11:43:19 -0700

    added panel to mountain result

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
   def user_search
-    @mountains = mountain_search(params[:search].capitalize)
+    @mountains = mountain_search(params[:search])
     @peaks = peak_search(params[:search])
     @trails = trail_search(params[:search])
     render :user_search
@@ -10,29 +10,14 @@ class SearchController < ApplicationController
 private
 
   def mountain_search(search)
-    if search
-      mountains = Mountain.where('name ILIKE ?', "%#{search}%")
-    else
-      Mountain.find(:all)
-    end
-    mountains
+    mountains = Mountain.where('name ILIKE ?', "%#{search}%")
   end
 
   def peak_search(search)
-    if search
-      peaks = Peak.where('name ILIKE ?', "%#{search}%")
-    else
-      "nothing, man"
-    end
-    peaks
+    peaks = Peak.where('name ILIKE ?', "%#{search}%")
   end
 
   def trail_search(search)
-    if search
-      trails = Trail.where('name ILIKE ?', "%#{search}%")
-    else
-      "nothing, man"
-    end
-    trails
+    trails = Trail.where('name ILIKE ?', "%#{search}%")
   end
 end
