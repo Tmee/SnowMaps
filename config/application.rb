18f050e62e4ccaeaba03a9a3bff1bb147d108cd3
Timothy Mee <tjmee90@gmail.com>
2015-01-22 11:08:18 -0700

    assets

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+config.assets.paths << Rails.root.join("app", "assets", "foundation-icons")
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,5 +11,6 @@ Bundler.require(*Rails.groups)
 module SnowMaps
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "foundation-icons")
   end
 end
