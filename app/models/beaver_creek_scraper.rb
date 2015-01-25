12437d33e6657d2eb4b108965064be46619335fa
Timothy Mee <tjmee90@gmail.com>
2015-01-24 23:49:37 -0700

    add some snowfall css to show, not sure if its cool enough to make it.  also added some styles to layouts

--- app/models/beaver_creek_scraper.rb
+++ app/models/beaver_creek_scraper.rb
@@ -162,7 +162,7 @@ class BeaverCreekScraper < ActiveRecord::Base
     array.delete_at(0)
     array.each do |trail|
       trail[:open] = trail[:open].scan(/\b(noStatus|yesStatus)\b/).join(',')
-      trail[:difficulty] = trail[:difficulty].scan(/\b(easiest|moreDifficult|mostDifficult|doubleDiamond)\b/).join(',')
+      trail[:difficulty] = trail[:difficulty].scan(/\b(easiest|moreDifficult|mostDifficult|doubleDiamond|expert)\b/).join(',')
     end
   end
 end
