ac77b4a7c3419b1e0587fd901fb82aca965c0259
Timothy Mee <tjmee90@gmail.com>
2015-01-28 17:53:31 -0700

    testing

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe SearchController, :type => :controller do
 
+  describe "GET search" do
+    it "gets search results" do
+      get :user_search
+    end
+  end
 end
